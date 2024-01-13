07b131c2e35795f9eea4c4539b5080695f64feca
LouisZhong <destinyxll@gmail.com>
2024-01-13 12:31:09 +0800

    feat: add hbs

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
-
+const exphbs = require('express-handlebars')
 const port = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
+app.set('view engine', 'hbs')
+
+
 app.get('/', (req, res) => {
-  res.send(`This is my first Express Web App`)
+  res.render('index')
 })
 
 app.listen(port, () => {
-  console.log(`This is my first Express Web App started by nodemon`)
+  console.log(`Express is listening on localhost:${port}`)
 })
